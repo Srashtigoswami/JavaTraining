6df32b85ccde336d9cc6b7a3eb3af8cb5b2f77cd
Srashtigoswami <Srashtigoswami96@gmail.com>
2026-04-24 15:06:35 +0530

    24 april

---
+++ Parking.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Parking {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter total no. of parking  hours:");
+        double hours=sc.nextDouble();
+        double bill;
+
+    if (hours<=2){
+        bill=hours*100;
+
+    }
+    else if(hours<=5){
+        bill=2*100+(hours-2)*50;
+
+    }
+    else {
+        bill=2*100+3*50+(hours-5)*25;
+    }
+    System.out.println("Total parking bill: "+bill);
+        sc.close();
+    }
+    
+}
